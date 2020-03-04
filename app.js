540946f585183482de87107503f6c0e9a0f30957
John <ljonghan@gmail.com>
2020-03-03 20:44:54 -0500

    basic

--- app.js
+++ app.js
@@ -33,6 +33,7 @@ orm.viewDepartment(function(result){
 */
 
 var inquirer = require("inquirer");
+var ui = new inquirer.ui.BottomBar();
 
 var employeeArr = []; 
 
@@ -51,11 +52,11 @@ function prompt() {
 			]
 		},
 	]).then(res => {
-		addToDB(res);
+		accessDB(res);
 	})
 }
 
-function addToDB(answers){
+function accessDB(answers){
 	switch(answers.main ) {
 		case "Add":
 			inquirer.prompt([
@@ -70,16 +71,113 @@ function addToDB(answers){
 				]
 			}
 			]).then(res => {
-				addDepartment(res)
+				addToDb(res)
 			})
 			break;
-
+		case "View": 
+			inquirer.prompt([
+			{
+				type: "list",
+				message: "What to view?",
+				name: "main",
+				choices: [
+					"Department", 
+					"Roles", 
+					"Employees"
+				]
+			}
+			]).then(res => {
+				viewDb(res)
+			})
+			break;	
+		case "Update": 
+			updateRole();
+			break;
 		default: 	
 			break; 
 	} 
 }
+function updateRole(){
+	orm.viewEmployees((res)=>{
+		var employees = []
+		var viewEmp = []
 
-function addDepartment(answers){
+		res.forEach((ele)=>{
+			viewEmp.push(ele.id + ' | ' + ele.first_name + ' ' + ele.last_name)
+			employees.push(ele.id)
+		})
+		
+		inquirer.prompt([
+		{
+			type: "list",
+			message: "Select Employee",
+			name: "main",
+			choices: viewEmp	
+		}
+		]).then((c) => {
+
+			orm.viewRoles((res)=>{
+				var roles = []
+				res.forEach((ele)=>{
+					roles.push(ele.id + ' | ' + ele.title)				
+				})
+				inquirer.prompt([
+				{
+					type: "list",
+					name: "main",
+					message: "Select New Role",
+					choices: roles
+				},
+				]).then(res => {
+					orm.updateRole(res.main.split("|")[0], (ans)=>{
+						prompt();
+					})
+				})
+			})
+		})	
+	})
+}
+function viewDb(answers){
+	switch(answers.main){
+		case "Department": 
+			var departments = []
+			orm.viewDepartment((res)=>{
+				res.forEach((ele)=>{
+					orm.viewBudget(ele.id, (bud)=>{
+						departments.push([ele.name, bud[0]['sum(salary)']])
+						if(departments.length === res.length){
+							departments.forEach((ele)=>{
+								console.log(ele[0] + ' | $' + ele[1]); 
+							})
+							prompt()
+						}
+					})
+				})
+			})
+			break; 
+		case "Roles":
+			var departments = []
+			orm.viewRoles((res)=>{
+				res.forEach((ele)=>{
+					console.log(ele.id + ' | '+ ele.title)
+				})
+				prompt()
+			})
+			break; 
+		case "Employees": 
+			orm.viewEmployees((res)=>{
+				res.forEach((ele)=>{
+					console.log(ele.id + ' | ' + ele.first_name + ' ' + ele.last_name)
+				})
+				prompt()
+			})
+			break; 
+
+		default: 
+			break; 
+	}
+}
+function addToDb(answers){
 	switch(answers.main ) {
 		case "Department":
 			inquirer.prompt([
@@ -98,6 +196,80 @@ function addDepartment(answers){
 				prompt();
 			})
 			break;
+
+		case "Roles":
+
+			var departments = []
+			orm.viewDepartment((res)=>{
+				res.forEach((ele)=>{
+					departments.push(ele.id + ' | '+ ele.name)
+				})
+				inquirer.prompt([
+				{
+					type: "input",
+					name: "id",
+					message: "Role ID?"
+				},
+				{
+					type: "input",
+					name: "title",
+					message: "Role Title?"
+				},
+				{
+					type: "input",
+					name: "salary",
+					message: "Role Salary?"
+				},
+				{
+					type: "list",
+					name: "dep",
+					message: "Department?",
+					choices: departments
+				},
+
+				]).then(r => {
+					orm.addRole(r.id, r.title, r.salary, r.dep.split("|")[0],(res)=>{
+						console.log(res)
+						prompt();
+					})
+				})
+			})
+			break; 
+		case "Employees":
+			var roles = []
+			orm.viewRoles((res)=>{
+				res.forEach((ele)=>{
+					roles.push(ele.id + ' | '+ ele.title)
+				})
+				inquirer.prompt([
+		
+				{
+					type: "input",
+					name: "first",
+					message: "Employee First Name?"
+				},
+				{
+					type: "input",
+					name: "last",
+					message: "Employee Last Name?"
+				},
+				{
+					type: "list",
+					name: "role",
+					message: "Role?",
+					choices: roles
+				},
+
+				]).then(r => {
+					console.log(r)
+					orm.addEmployee(r.first, r.last, r.role.split("|")[0],null,(res)=>{
+						console.log(res)
+						prompt();
+					})
+				})
+			})
+			break; 
+			break; 
 		default: 	
 			break; 
 	} 
--- config/orm.js
+++ config/orm.js
@@ -26,21 +26,28 @@ const orm = {
         if(err.code == 'ER_DUP_ENTRY' || err.errno == 1062){
           cb('Duplicate')
         } 
+      } else {
+         cb('done')
       }
     });
   },
   addEmployee: function(first_name, last_name, role_id, manager_id,cb) {
     const queryString = "INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?,?,?,?)";
     connection.query(queryString, [first_name, last_name, role_id, manager_id], function (err, result) {
+      console.log(result)
       if (err) {
         if(err.code == 'ER_DUP_ENTRY' || err.errno == 1062){
           cb('Duplicate')
-        } 
+        } else{
+          cb(err)
+        }
+      }else{
+        cb('Done')
       }
     });
   },
   viewDepartment: function(cb){
-    const queryString =`SELECT name FROM department`;
+    const queryString =`SELECT id,name FROM department`;
     connection.query(queryString,[],function (err, result) {
       if (err) {
         if(err.code == 'ER_DUP_ENTRY' || err.errno == 1062){
@@ -50,7 +57,51 @@ const orm = {
       cb(result)
     });
   },
-
+  viewEmployees: function(cb){
+    const queryString =`SELECT id,first_name,last_name FROM employee`;
+    connection.query(queryString,[],function (err, result) {
+      if (err) {
+        cb('error')
+      }
+      cb(result)
+    });
+  },
+  viewBudget: function(index, cb){
+    const queryString =
+    `SELECT sum(salary) FROM department, employee, role 
+    WHERE department.id = ? and 
+          department.id = role.department_id and 
+          employee.role_id = role.id`;
+    connection.query(queryString,[index],function (err, result) {
+      if (err) {
+        if(err.code == 'ER_DUP_ENTRY' || err.errno == 1062){
+          cb('Duplicate')
+        } 
+      }
+      cb(result)
+    });
+  },
+  viewRoles: function(cb){
+    const queryString =`SELECT id,title FROM role`;
+    connection.query(queryString,[],function (err, result) {
+      if (err) {
+        cb('error')
+      }
+      cb(result)
+    });
+  },
+  updateRole: function(index, cb){
+    const queryString =
+    'UPDATE employee SET first_name = "test" WHERE id = ?';
+    connection.query(queryString,[index],function (err, result) {
+      if (err) {
+        if(err.code == 'ER_DUP_ENTRY' || err.errno == 1062){
+          cb('Duplicate')
+        } 
+      }
+      cb(result)
+    });
+  },
 //SELECT salary FROM department, employee, role WHERE department.id = 1 and department.id = role.department_id and employee.role_id = role.id; 
 // /UPDATE employee SET first_name = 'test' WHERE id = 1;
 };
